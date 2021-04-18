87b38289ecd80058670b3457fe87e435b87a0be4
Sebastien Leboucher <sebastien.leboucher@gmail.com>
2021-04-18 18:41:18 +0200

    pool

--- web/src/main/java/com/exemple/web/Config.java
+++ web/src/main/java/com/exemple/web/Config.java
@@ -3,16 +3,46 @@
  */
 package com.exemple.web;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Config {
 
     @Bean
-    RestTemplate restTemplate() {
+    RestTemplate restTemplate() throws InterruptedException {
         return new RestTemplate();
+//        return new RestTemplate(createRequestFactory());
+    }
+
+
+    /**
+     * The default HttpClient used in the RestTemplate is provided by the JDK. It is developed on top of the HttpURLConnection.
+     * There is a new module added in Java 9 in incubation status and standardized in Java 11 called java.net.http.HttpClient.
+     */
+    private ClientHttpRequestFactory createRequestFactory() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(400);
+        connectionManager.setDefaultMaxPerRoute(400);
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(5000)
+                .setSocketTimeout(10000)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
 
 //    @Bean
