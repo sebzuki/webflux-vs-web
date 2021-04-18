3c9b731b3cc66e2c4013923d8bfec4e5197aee2d
Sebastien Leboucher <sebastien.leboucher@gmail.com>
2021-04-18 19:19:47 +0200

    error

--- webflux/src/main/java/com/exemple/webflux/dao/impl/RestRepositoryImpl.java
+++ webflux/src/main/java/com/exemple/webflux/dao/impl/RestRepositoryImpl.java
@@ -5,6 +5,7 @@ package com.exemple.webflux.dao.impl;
 
 import com.exemple.webflux.dao.RestRepository;
 import com.exemple.webflux.dao.domain.Student;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -26,6 +27,10 @@ public class RestRepositoryImpl implements RestRepository {
         return webClient.get()
                 .uri("http://localhost:8083/load")
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError,
+                        error -> Mono.error(new RuntimeException("API not found")))
+                .onStatus(HttpStatus::is5xxServerError,
+                        error -> Mono.error(new RuntimeException("Server is not responding")))
                 .bodyToMono(Student[].class)
                 .map(Arrays::asList);
     }
