19509cc186ca24528230c709d058dbc0973733c1
Sebastien Leboucher <sebastien.leboucher@gmail.com>
2021-04-19 10:13:54 +0200

    code

--- webflux/src/main/java/com/exemple/webflux/dao/impl/RestRepositoryImpl.java
+++ webflux/src/main/java/com/exemple/webflux/dao/impl/RestRepositoryImpl.java
@@ -5,6 +5,7 @@ package com.exemple.webflux.dao.impl;
 
 import com.exemple.webflux.dao.RestRepository;
 import com.exemple.webflux.dao.domain.Student;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -31,7 +32,16 @@ public class RestRepositoryImpl implements RestRepository {
                         error -> Mono.error(new RuntimeException("API not found")))
                 .onStatus(HttpStatus::is5xxServerError,
                         error -> Mono.error(new RuntimeException("Server is not responding")))
-                .bodyToMono(Student[].class)
-                .map(Arrays::asList);
+                .bodyToMono(new ParameterizedTypeReference<>() {});
+
+        //        return webClient.get()
+//                .uri("http://localhost:8083/load")
+//                .accept(MediaType.APPLICATION_JSON)
+//                .exchangeToFlux(response -> {
+//                    if (response.statusCode().equals(HttpStatus.OK)) {
+//                        return response.bodyToFlux(Student.class);
+//                    }
+//                    return Flux.empty();
+//                });
     }
 }
